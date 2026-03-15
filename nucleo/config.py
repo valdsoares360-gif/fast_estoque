d7caed8ea1d7d87c7afecdd5e642a770be3e1bf0
valdsoares360-gif <vald.soares360@gmail.com>
2026-03-15 19:06:20 -0300

    Corrigido config.py e variáveis de ambiente para JWT

--- nucleo/config.py
+++ nucleo/config.py
@@ -4,5 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY não definida! Verifique o .env ou as Environment Variables no Render.")
+
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 120))
